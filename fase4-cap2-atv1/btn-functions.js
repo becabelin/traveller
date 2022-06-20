05dfe16f874d724f6645c852a5f77f7f4ed05b96
Rebeca Belin <becabelin@gmail.com>
2022-06-19 21:34:52 -0300

    separando functions

---
+++ fase4-cap2-atv1/btn-functions.js
@@ -0,0 +1,39 @@
+function nomeDasCidades() {
+    alert("São Paulo, Las Vegas e Moscou");
+  }
+  
+  function roteiroADeSaoPaulo() {
+    alert("Avenida Paulista: MASP, Parque Trianon e Rua Augusta");
+  }
+  
+  function roteiroADeLasVegas() {
+    alert(
+      "Las Vegas Boulevard South: Fonte do Bellagio, Principais Cassinos e Madame Tussauds"
+    );
+  }
+  
+  function roteiroADeMoscou() {
+    alert(
+      "Praça Vermelha: Museu Histórico do Estado, Catedral de São Basílico e Mausoléu de Lênin"
+    );
+  }
+  
+  function qtdLocaisSaoPaulo() {
+    alert("3");
+  }
+  
+  function qtdLocaisLasVegas() {
+    alert("3");
+  }
+  
+  function qtdLocaisMoscou() {
+    alert("3");
+  }
+  
+  function nomePontosCentro() {
+    alert("Catedral da Sé, Pátio do Colégio e Rua Augusta");
+  }
+  
+  function nomePontosDowntown() {
+    alert("Fremont, Las Vegas Art Museum e Museu Nacional do Crime Organizado");
+  }
